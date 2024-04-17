7e0b9f08a28eafc5a1fb1ba373b5be976e38779d
Maskani18 <165617766+Maskani18@users.noreply.github.com>
2024-04-17 19:59:59 +0700

    Add files via upload

---
+++ dip/KipasAngin.php
@@ -0,0 +1,13 @@
+<?php
+include_once("BarangElektronik.php");
+
+class KipasAngin implements BarangElektronik {
+    public function beroperasi(): void {
+        echo "Kipas angin menyala.\n";
+    }
+
+    public function berhenti(): void {
+        echo "Kipas angin dimatikan.\n";
+    }
+}
+?>
---
+++ dip/index.php
@@ -0,0 +1,22 @@
+<?php
+include_once("Lampu.php");
+include_once("KipasAngin.php");
+include_once("Saklar.php");
+
+$lampu = new Lampu();
+$kipasAngin = new KipasAngin();
+
+$saklarLampu = new Saklar($lampu);
+
+$saklarKipasAngin = new Saklar($kipasAngin);
+
+echo "Menggunakan saklar untuk lampu:\n";
+$saklarLampu->saklar();
+$saklarLampu->berubah();
+$saklarLampu->berubah();
+
+echo "\nMenggunakan saklar untuk kipas angin:\n";
+$saklarKipasAngin->saklar();
+$saklarKipasAngin->berubah();
+$saklarKipasAngin->berubah();
+?>
